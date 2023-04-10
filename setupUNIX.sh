c1cafe5589ed737245e90774ffab357575d15bda
grava <gravatharg@gmail.com>
2023-04-10 17:39:13 +0500

    added script to automatically setup environment on Linux/MacOS

--- setupUNIX.sh
+++ setupUNIX.sh
@@ -1,16 +1,4 @@
-## Installation
-Windows (PowerShell, cmd):
-```sh
-python -m venv venv
-venv\Scripts\activate
-pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py createsuperuser
-python manage.py runserver
-```
-Linux/MacOS (Bash, Zsh, etc.):
-```sh
+#!/bin/bash
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
@@ -21,4 +9,3 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser
 python manage.py runserver
-```
