c32893edf540af8939e29e4982d7ed0850ac17bd
Alex <>
2023-04-11 12:24:04 +0500

    notifications added, request, response pages are ready

--- app/forms.py
+++ app/forms.py
@@ -3,12 +3,24 @@ from django import forms
 from .models import ServiceRequest, ServiceResponse
 
 
-class ServiceRequestForm(forms.ModelForm):
-    class Meta:
-        model = ServiceRequest
-        fields = '__all__'
+class ServiceRequestForm(forms.Form):
+    title = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea)
+    price_from = forms.DecimalField(min_value=0)
+    price_to = forms.DecimalField(min_value=0)
+    place = forms.CharField(widget=forms.Textarea)
+    term = forms.DecimalField(min_value=0)
         
-        
+    def __init__(self, data, user, *args, **kwargs):
+        super(ServiceRequestForm, self).__init__(data, *args, **kwargs)
+        self.customer = user
+    
+    def save(self, commit=True):
+        return ServiceRequest.objects.create(
+            **self.cleaned_data,
+            customer=self.customer,
+        )
+    
 class ServiceResponseForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
     def __init__(self, data, service_request, user, *args, **kwargs):
--- app/views.py
+++ app/views.py
@@ -1,28 +1,77 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, FormView
+from django.views.generic import (ListView, DetailView, FormView, 
+                                  TemplateView, RedirectView)
 from django.views.generic.detail import SingleObjectMixin
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import ServiceRequest, ServiceResponse
 from .forms import ServiceRequestForm, ServiceResponseForm
 
 
-class ServiceRequestListView(ListView):
+class MainView(RedirectView):
+    def get_redirect_url(self):
+        if self.request.user.is_authenticated:
+            return reverse_lazy('app:request_list')
+        return reverse_lazy('app:info')
+
+
+class InfoView(TemplateView):
+    template_name = 'app/info.html'
+
+
+class ServiceRequestListView(LoginRequiredMixin, ListView):
     model = ServiceRequest
     template_name = 'app/request_list.html'
     context_object_name = 'requests'
     
     
-class ServiceRequestDetailView(DetailView):
+class ServiceRequestDetailView(LoginRequiredMixin, DetailView):
     model = ServiceRequest
     template_name = 'app/request_detail.html'
     context_object_name = 'request'
     
-class ServiceResponseCreateView(SingleObjectMixin, FormView):
+    
+class ServiceRequestCreateView(LoginRequiredMixin, FormView):
+    model = ServiceRequest
+    template_name = 'app/request_create.html'
+    form_class = ServiceRequestForm
+    success_url = reverse_lazy('app:main')
+    
+    def get_form(self, form_class=None):
+            form = self.form_class(self.request.POST or None,
+                                user=self.request.user)
+            return form
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+class ServiceResponseListView(LoginRequiredMixin, ListView):
+    model = ServiceResponse
+    template_name = 'app/response_list.html'
+    context_object_name = 'responses'
+
+
+class ServiceResponseDetailView(LoginRequiredMixin, DetailView):
+    model = ServiceResponse
+    template_name = 'app/response_detail.html'
+    context_object_name = 'response'
+    
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.watched = True
+        obj.save()
+        return obj
+    
+    
+
+
+class ServiceResponseCreateView(LoginRequiredMixin, SingleObjectMixin, FormView):
     model = ServiceRequest
     template_name = 'app/response_create.html'
     form_class = ServiceResponseForm
-    success_url = reverse_lazy('app:main')
+    success_url = reverse_lazy('app:response_list')
     
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
@@ -41,3 +90,16 @@ class ServiceResponseCreateView(SingleObjectMixin, FormView):
                                service_request=self.object,
                                user=self.request.user)
         return form
+    
+    
+class NotificationsView(ListView):
+    model = ServiceResponse
+    template_name = 'app/notifications.html'
+    context_object_name = 'responses'
+    
+    def get_queryset(self):
+        queryset = self.model.objects.filter(
+            service_request__customer=self.request.user,
+            watched=False
+        )
+        return queryset
