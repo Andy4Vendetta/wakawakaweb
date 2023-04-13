faf752c5e1b535ef40e4fd5a8f39176787b93dc8
grava <gravatharg@gmail.com>
2023-04-13 11:52:13 +0500

    fixed collectstatic

--- setupUNIX.sh
+++ setupUNIX.sh
@@ -6,6 +6,7 @@ echo DEBUG=True >> .env
 ABOB=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
 echo SECRET_KEY=$ABOB >> .env
 echo ALLOWED_HOSTS=localhost,127.0.0.1,grava.tharg.ru,hack.tharg.ru,79.137.207.11,109.195.147.68 >> .env
+python3 manage.py collectstatic
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser
